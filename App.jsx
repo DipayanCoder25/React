e25628736700a12494819921b172e41835f64658
DipayanCoder25 <dipayanprogramer3@gmail.com>
2026-03-03 12:36:28 +0600

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,19 +1,74 @@
-import { useState } from 'react'
+import React, { useState } from "react";
+import "./App.css";
 
-import React from 'react'
+function App() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    age: "",
+  });
 
-import reactLogo from './assets/react.svg'
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-import viteLogo from '/vite.svg'
+  const [message, setMessage] = useState("");
 
-import './App.css'
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password || !formData.age) {
+      setFormData("All fields r fulled");
+      return;
+    }
+    if (
+      formData.email === "dip@gmail.com" &&
+      formData.password === "admin" &&
+      formData.age === "25"
+    ) {
+      setMessage("Login Succesful");
+    } else {
+      setMessage("Invalid email or passoword");
+    }
+  };
 
-function App(){
-	return(
-		<div>
-			<h1>Hello</h1>
-		</div>
-		);
+  return (
+    <div className="container">
+      <h1>Login Form</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <input
+          type="age"
+          name="age"
+          placeholder="Enter age"
+          value={formData.age}
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+
+      {message && <p>{message}</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
